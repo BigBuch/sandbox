69d735b0fd2f41a04335831dc5152163bb5bb849
ASTOUND\r.bulanov <r.bulanov@astoundcommerce.com>
2022-12-28 23:45:22 +0200

    Weather app ES6

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,16 +4,14 @@ const key = require('../privatekeys/keys.js').mapbox_key;
 const geocode = (address, callback) => {
     const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${key}&limit=1`;
     request({ url: geoUrl, json: true }, (error, response) => {
+        const { features = [] } = response.body
         if (error) {
             callback('unable to connect', undefined)
-        } else if (!response.body.features || response.body.features.length === 0) {
+        } else if (features.length === 0) {
             callback(response.body.message || 'location not found', undefined);
         } else {
-            callback(undefined, {
-                lon: response.body.features[0].center[0],
-                lat: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
-            });
+            const { center: [lon, lat], place_name: location } = features[0]
+            callback(undefined, { lon, lat, location });
         }
     })
 }
